43e6080edc8ca9fdec6b194fc8a235c04ef37fb5
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-20 15:06:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
 const config = require("./config.json"); //Defining the place where we will store our swears
-client.snipes = new Discord.Collection();
 
 client.once("ready", () => {
   console.log('Filter Bot Online! Created by CustomNate#0494')
@@ -60,53 +59,9 @@ client.on('message', message => {
   }
 })
 
-client.on('messageDelete', message => {
-  try {
-    if (message.author.bot) return;
-    const snipes = message.client.snipes.get(message.channel.id) || [];
-    snipes.unshift({
-      content: message.content,
-      author: message.author,
-      image: message.attachments.first()
-        ? message.attachments.first().proxyURL
-        : null,
-      date: new Date().toLocaleString("en-GB", {
-        dataStyle: "full",
-        timeStyle: "short",
-      }),
-    });
-    snipes.splice(10);
-    message.client.snipes.set(message.channel.id, snipes);
-    let embed = new MessageEmbed()
-      .setTitle(`New message deleted!`)
-      .setDescription(
-        `**The user ${message.author.tag} has deleted a message in <#${message.channel.id}>**`
-      )
-      .addField(`Content`, message.content, true)
-      .setColor(`RED`);
-    let channel = message.guild.channels.cache.find(
-      (ch) => ch.name === "bot-log"
-    );
-    if (!channel) return;
-    channel.send(embed);
-  } catch (e) {}
-})
-
-client.on('message', message => {
-  if (message.content.toLowerCase() === "christ help") {
-    const snipes = client.snipes.get(message.channel.id) || [];
-    const msg = snipes[args[0] - 1 || 0];
-    if (!msg) return message.channel.send(`That is not a valid snipe...`);
-    const Embed = new MessageEmbed()
-      .setAuthor(
-        msg.author.tag,
-        msg.author.displayAvatarURL({ dynamic: true, size: 256 })
-      )
-      .setDescription(msg.content)
-      .setFooter(`Date: ${msg.date} | ${args[0] || 1}/${snipes.length}`);
-    if (msg.attachment) Embed.setImage(msg.attachment);
-    message.channel.send(Embed);
-  }
+client.on('guildMemberAdd', guildMember => {
+  let epicRole = message.guild.roles.cache.get('757241153207795812')
+  guildMember.roles.add(epicRole)
 })
 
 client.login(process.env.token);
