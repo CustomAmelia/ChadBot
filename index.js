558ff59502d6d8513af2905f4a66a318ee4749ca
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-20 14:43:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -59,4 +59,21 @@ client.on('message', message => {
   }
 })
 
+client.on('message', message => {
+  if (message.content.toLowerCase() === "christ help") {
+    const snipes = client.snipes.get(message.channel.id) || [];
+    const msg = snipes[args[0] - 1 || 0];
+    if (!msg) return message.channel.send(`That is not a valid snipe...`);
+    const Embed = new MessageEmbed()
+      .setAuthor(
+        msg.author.tag,
+        msg.author.displayAvatarURL({ dynamic: true, size: 256 })
+      )
+      .setDescription(msg.content)
+      .setFooter(`Date: ${msg.date} | ${args[0] || 1}/${snipes.length}`);
+    if (msg.attachment) Embed.setImage(msg.attachment);
+    message.channel.send(Embed);
+  }
+})
+
 client.login(process.env.token);
