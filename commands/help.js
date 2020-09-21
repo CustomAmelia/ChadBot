22d9c9a7d7cd59784ffdf5eebe47865234104381
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-21 22:06:36 +0100

    f

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+module.exports.run = async (bot, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Chad Bot || Help')
+    .setDescription('Here you can find a full list of the commands you can use along with some cool tips!')
+    .addFields(
+		{ name: 'help', value: 'Gives you a list of commands.', inline: true },
+		{ name: 'test', value: 'test', inline: true },
+	)
+}
+
+module.exports.config = {
+    name: "help",
+    description: "Here you can find a full list of the commands you can use along with some cool tips!",
+    usage: "++help",
+    aliases: ['cmds']
+}
