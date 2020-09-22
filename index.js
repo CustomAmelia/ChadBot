000d50a600679d0a4fd414d71526125d7c0ac3b1
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-22 21:46:37 +0100

    f

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
 const botsettings = require('./botsettings.json');
-
 const bot = new Discord.Client({disableEveryone: true});
+const mongoose = require('mongoose')
+const prefix = '../models/prefix'
 
 bot.on("ready", () => {
   console.log(`${bot.user.username} is online`)
   bot.user.setActivity("PLACEHOLDER", {type: 'PLAYING'});
 })
+
+mongoose.connect('mongodb+srv://Brady1290:caniver1234@cluster0.bf245.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const fs = require("fs");
 bot.commands = new Discord.Collection();
@@ -33,15 +36,30 @@ fs.readdir("./commands/", (err, files) => {
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
 
-    let prefix = botsettings.prefix;
+    const data = await prefix.findOne({
+        GuildID: message.guild.id
+    })
+
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0].toLowerCase()
     let args = messageArray.slice(1);
 
+    if (data) {
+
+    const prefix = data.Prefix
+
     if(!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
     if(commandfile) commandfile.run(bot,message,args)
+    }
+    else if (!data) {
 
+        const prefix = '++'
+
+        if(!message.content.startsWith(prefix)) return;
+        let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+        if(commandfile) commandfile.run(bot,message,args)
+    }
 })
 
 bot.login(process.env.token);
