dbd47a7390a839b855b60c0269ba1ddac81a9264
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-22 16:39:12 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,8 @@ module.exports.run = (bot, message, args) => {
     embed.setDescription('Here you can find a full list of the commands you can use along with some cool tips!')
     embed.addFields(
 		{ name: 'help', value: 'Gives you a list of commands.', inline: true },
-		{ name: 'test', value: 'test', inline: true },
+    { name: 'hack <person>', value: 'Does a fake hack on the person you mention.', inline: true },
+    { name: 'meme', value: 'Posts a random meme from any of the following subreddits: r/meme, r/me_irl, r/dankmeme, r/nocontextxboxmessages', inline: true },
     )
     embed.setColor('#0099ff')
     
