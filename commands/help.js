a4530701da00b2af1931aac993d7eee08bfee8ee
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-23 15:32:19 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,7 +36,6 @@ module.exports.run = (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
     embed.setTitle('**Chad Bot || Help**')
     embed.setColor('#57b9ff')
-    embed.setFooter('++help <category> or custom server prefix before the help <category> cmd!')
     embed.addFields({
       name: "**Fun** 😂",
       value: "``help fun``",
