6c11b8ee81d060adcf1d0ba6b11eb7a5ccd9e3f4
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-24 20:02:35 +0100

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
 
     const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard); 
 
-    const lb = leaderboard.map(e => ` ``\ ${e.position} • ${e.username}#${e.discriminator}\nChad Level: ${e.level}\nXP: ${e.xp.toLocaleString()}\ ```)
+    const lb = leaderboard.map(e => `${e.position} • ${e.username}#${e.discriminator}\nChad Level: ${e.level}\nXP: ${e.xp.toLocaleString()}`)
     const embed = new Discord.MessageEmbed()
     .setTitle(`**${message.guild.name}'s Leaderboard**`)
     .setDescription(`${lb.join("\n\n")}`)
