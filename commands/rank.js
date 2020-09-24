44ad3fd9042d381264eadede599223c7f1ad7cb7
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-24 21:07:02 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -1,11 +1,23 @@
 const Discord = require('discord.js')
-const Levels = require('discord-xp')
+const Levels = require('discord-xp');
+const { prependOnceListener } = require('discord-xp/models/levels');
 
 module.exports.run = async (bot, message, args) => {
     const user = await Levels.fetch(message.author.id, message.guild.id);
+    const person = message.mentions.users.first();
     
+    if (!person) {
     const embed = new Discord.MessageEmbed()
     .setTitle(`**${message.author.username}'s Level**`)
+    .addField('Level', user.level)
+    .addField('XP', user.xp)
+    .setTimestamp()
+    .setColor("#57b9ff")
+    message.channel.send(embed)
+    }
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle(`**${person.username}'s Level**`)
     .addField('Level', user.level)
     .addField('XP', user.xp)
     .setTimestamp()
