1cae8e7b9753c699af97c4faf2ec16875a422368
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-24 22:56:14 +0100

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -5,8 +5,6 @@ module.exports.run = async (bot, message, args) => {
     const user = await Levels.fetch(message.author.id, message.guild.id);
     const person = message.mentions.users.first();
 
-    if (person.bot) return;
-
     if (!person) {
         const embed = new Discord.MessageEmbed()
         .setTitle(`**${message.author.username}'s Level**`)
