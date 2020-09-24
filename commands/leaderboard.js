7ff83639f09fce73137ba3fe2d0fb7ec334d91cc
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-24 22:20:45 +0100

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,7 +4,7 @@ const Levels = require('discord-xp')
 module.exports.run = async (bot, message, args) => {
     const user = await Levels.fetch(message.author.id, message.guild.id);
     
-    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
+    const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10);
     if (rawLeaderboard.length < 1) return reply("Nobody's on the chad leaderboard yet.");
 
     const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard); 
@@ -20,7 +20,7 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.config = {
     name: "leaderboard",
-    description: "Gives you the list of the 5 highest chad levels in the server!",
+    description: "Gives you the list of the 10 highest chad levels in the server!",
     usage: "++leaderboard",
     aliases: ["lb"]
 }
