b3a1aa0f4065329b0dc48ba8d497423aaf1e85da
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-25 15:38:11 +0100

    Create saw.js

---
+++ commands/saw.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    const person = message.mentions.users.first();
+
+    if (person.bot) return;
+
+    if (!person) return message.channel.send("Who do I saw in half?")
+
+    message.channel.send(`*saws ${person.username} in half*`)
+}
+
+module.exports.config = {
+    name: "saw",
+    description: "I sawed this person in half!",
+    usage: "++saw <person>",
+    aliases: ["lb"]
+}
