b9e40eb0bbbbe6be4385b8b03d6633bd192f5b58
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-26 00:03:11 +0100

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
 
     if (args[0].length > 5) return message.channel.send('Your new prefix must be under \`5\` characters!')
 
-    if (message.author.hasPermission('MANAGE_SERVER')) {
+    if (message.member.hasPermission('MANAGE_SERVER')) {
     if (data) {
         await prefixModel.findOneAndRemove({
             GuildID: message.guild.id
@@ -35,7 +35,7 @@ module.exports.run = async (bot, message, args) => {
         newData.save();
     }
 }
-else if (!message.author.hasPermission('MANAGE_SERVER')) {
+else if (!message.member.hasPermission('MANAGE_SERVER')) {
     message.channel.send('uh oh! you do not have the required permissions to run this command, you need the MANAGE_SERVER permission!')
 }
 }
