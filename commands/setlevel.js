314e59f4da47668865d3730f6189ce9504ecefe0
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-25 18:04:38 +0100

    Update setlevel.js

--- commands/setlevel.js
+++ commands/setlevel.js
@@ -3,7 +3,9 @@ const Levels = require('discord-xp')
 
 module.exports.run = async (bot, message, args) => {
 
+    if (message.author.id === '498097065264676864') {
     Levels.setLevel(message.author.id, message.guild.id, 1000);
+    }
 };
 
 module.exports.config = {
