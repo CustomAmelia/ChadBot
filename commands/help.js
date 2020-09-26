4f18846e555ddc1a7a14bb20191e408b6c4c21f7
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-26 14:07:50 +0100

    f

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
   if (args[0] === 'fun') {
     const embed = new Discord.MessageEmbed()
     embed.setTitle('**Chad Bot || Fun Commands**')
-    embed.setDescription('``hack <@person>``, ``meme``, ``frickword``')
+    embed.setDescription(' ``meme``, ``frickword``')
     embed.setColor('RANDOM')
     if (!data) {
       embed.setFooter('use ++ before each command')
