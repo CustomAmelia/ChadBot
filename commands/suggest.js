91b9574e0cfd4d466ff5154a3207542b014f1252
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-27 13:29:56 +0100

    f

---
+++ commands/suggest.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const suggestionModel = require('../models/suggest')
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send('done')
+
+    let newData = new suggestionModel({
+        Suggestion: "test",
+    })
+    newData.save();
+}
+
+module.exports.config = {
+    name: "suggest",
+    description: "placeholder",
+    usage: "++suggest",
+    aliases: []
+};
