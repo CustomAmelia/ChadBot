37c7107c48b7cbc71e2190d593376f81af3d01a1
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-27 12:18:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ bot.on('message', async (message) => {
     const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomXp);
     if (hasLeveledUp) {
         const user = await Levels.fetch(message.author.id, message.guild.id);
-        message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
+        message.channel.send(`${message.author}, You leveled up to ${user.level}! Keep it going!`);
     }
 
     const data = await prefix.findOne({
