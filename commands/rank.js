18664a01872b2942d6676c8b31855f9ac58c13f6
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-27 12:30:06 +0100

    f

--- commands/rank.js
+++ commands/rank.js
@@ -2,8 +2,17 @@ const Discord = require('discord.js')
 const Levels = require('discord-xp')
 
 module.exports.run = async (bot, message, args) => {
-    const user = await Levels.fetch(message.author.id, message.guild.id);
-    message.channel.send(`You are currently level **${user.level}**!`)
+    const person = message.mentions.users.first();
+
+    if (!person) {
+        const user = await Levels.fetch(message.author.id, message.guild.id);
+        message.channel.send(`You are currently level **${user.level}**!`)
+    }
+    if (person) {
+        if (person.bot) return;
+        const user = await Levels.fetch(person.id, message.guild.id);
+        message.channel.send(`${person.username} is currently level **${user.level}**!`)
+    }
 }
 
 module.exports.config = {
