0237abf81c4db449fa07c5345bb6ab158781019e
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-28 17:14:33 +0100

    Update wl.js

--- commands/wl.js
+++ commands/wl.js
@@ -3,13 +3,27 @@ const wl = require('../models/wl')
 
 module.exports.run = async (bot, message, args) => {
 
-    let newData = new wl({
+    const data = await wl.findOne({
         GuildID: message.guild.id
-    })
+    });
 
-    message.channel.send('Whitelisted!')
+    if (data) {
+        await wl.findOneAndRemove({
+            GuildID: message.guild.id
+        })
+        
+        message.channel.send('You are already whitelisted!')
+    }
+    else if (!data) {
 
-    newData.save();
+        let newData = new wl({
+            GuildID: message.guild.id
+        })
+
+        message.channel.send('Whitelisted!')
+    
+        newData.save();
+    }
 }
 
 module.exports.config = {
