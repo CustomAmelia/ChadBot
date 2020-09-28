93754f6d3d7d6274afe6e4a74b2867b735149313
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-28 17:19:03 +0100

    f

--- models/wl.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WlSchema = new mongoose.Schema({
-    GuildID: {
-        type: String
-    }
-});
-
-const MessageModel = module.exports = mongoose.model('whitelists', WlSchema);
