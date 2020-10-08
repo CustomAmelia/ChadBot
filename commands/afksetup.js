486f424e0f9b7d9e719fe555a2ffe88ac45a9a62
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-08 18:21:49 +0100

    Create afksetup.js

---
+++ commands/afksetup.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    let role = message.guild.roles.find(role => role.name === "AFK");
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('AFK Setup')
+    embed.setDescription('To setup the afk command, make a new role and name it "AFK" and then when you have done that, run this command again to see if it has been setup. Make sure I (ChadBot) has the Administrator permission!')
+    embed.setColor("RANDOM")
+
+    if (!role) return message.channel.send(embed)
+    if (role) return message.channel.send('The AFK command is already set up!')
+}
+
+module.exports.config = {
+    name: "afksetup",
+    description: "Setup the AFK command!",
+    usage: "++afksetup",
+    aliases: []
+};
