1a4846722b5aad7996e71b1c990a025a83351707
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-15 22:11:46 +0100

    Update setlbamount.js

--- commands/setlbamount.js
+++ commands/setlbamount.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
         if (!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('Uh Oh! You do not have the required permissions to run this command, you need the MANAGE_GUILD or ADMINISTRATOR permission!')
 
-        if (!args[0].length > 2) {
+        if (args[0].length > 2) {
             if (data) {
                 await lbamount.findOneAndRemove({
                     GuildID: message.guild.id
@@ -40,7 +40,7 @@ module.exports.run = async (bot, message, args) => {
                 newData.save();
             }
         }
-        else if (args[0].length > 2) {
+        else {
             message.channel.send('Amount must be under 10!')
         }
     }
