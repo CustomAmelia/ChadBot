8aeae2a1c19f37c3773d021c98aca3636468723e
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-18 15:20:47 +0100

    f

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -6,13 +6,8 @@ const lbamount = require('../models/lbamount')
 module.exports.run = async (bot, message, args) => {
     if(usedCommand.has(message.author.id)){
         message.reply('Slow down! You have to wait 2 seconds to use this command again.')
-    } else {
+    }
 
-        const data = await lbamount.findOne({
-            GuildID: message.guild.id
-        });
-
-    if (data) {
         const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, data.Amount);
 
         if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
@@ -27,26 +22,7 @@ module.exports.run = async (bot, message, args) => {
         embed.setColor("RANDOM")
     
         message.channel.send(embed)
-    }
-    else if (!data) {
-        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
-
-        if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
-
-        const leaderboard = Levels.computeLeaderboard(bot, rawLeaderboard);
-    
-        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
-    
-        const embed = new Discord.MessageEmbed()
-        embed.setTitle(`${message.guild.name}'s Leaderboard`)
-        embed.setDescription(lb.join("\n\n"))
-        embed.setColor("RANDOM")
-    
-        message.channel.send(embed).catch(error => {
-            message.channel.send(`LEADERBOARD[ERROR]: ${error} CHECK THAT YOUR LBAMOUNT IS NOT SET TO AN EXTREMELY HIGH AMOUNT LIKE 100, 10 OR 20 IS RECOMMENDED`)
-        })
-    }
-    }
+        
     usedCommand.add(message.author.id);
     setTimeout(() => {
         usedCommand.delete(message.author.id);
