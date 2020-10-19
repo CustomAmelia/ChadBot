d04aa07775883c5822d3fc6bb9a4170162d35711
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-19 21:13:45 +0100

    Create fakeperson.js

---
+++ commands/fakeperson.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const got = require('got')
+const botconfig = require("../botsettings.json");
+const usedCommand = new Set()
+
+module.exports.run = (bot, message, args) => {
+    if(usedCommand.has(message.author.id)){
+        message.reply('Slow down! You have to wait 2 seconds to use this command again.')
+    } else {
+        const embed = new Discord.MessageEmbed()
+        embed.setTitle('Fake Person!')
+        embed.setColor("RANDOM")
+        embed.setImage("https://thispersondoesnotexist.com/")
+
+        message.channel.send(embed)
+}
+usedCommand.add(message.author.id);
+setTimeout(() => {
+    usedCommand.delete(message.author.id);
+}, 2000);
+}
+
+module.exports.config = {
+    name: "fakeperson",
+    description: "Posts a random fake face!",
+    usage: "++fakeperson",
+    aliases: []
+}
