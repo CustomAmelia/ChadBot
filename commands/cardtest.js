dd8eef7784c8db948165747e8fc4b673e46d14fe
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-21 01:13:05 +0100

    bruh

---
+++ commands/cardtest.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js')
+const Canvas = require('canvas')
+const usedCommand = new Set()
+
+module.exports.run = async (bot, message, args) => {
+    if(usedCommand.has(message.author.id)){
+        message.reply('Slow down! You have to wait 2 seconds to use this command again.')
+    }
+
+    const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./canvas.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${message.member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(message.member.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'image.png');
+
+	channel.send(`Welcome to the server, ${message.author}!`, attachment);
+    
+    usedCommand.add(message.author.id);
+    setTimeout(() => {
+        usedCommand.delete(message.author.id);
+    }, 2000);
+}
+
+module.exports.config = {
+    name: "card",
+    description: "test",
+    usage: "++card",
+    aliases: []
+};
