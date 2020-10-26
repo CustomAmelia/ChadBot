ebea30e3f4e92c89fb73f73269a9df941cc71ebd
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-26 15:39:58 +0000

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const usedCommand = new Set()
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send("Pinging...").then(m =>{
+        var ping = m.createdTimestamp - message.createdTimestamp;
+        var botPing = Math.round(bot.pi);
+
+        m.edit(`**Pong! Bot Ping Is:-**\n  ${ping}ms`);
+    });
+}
+
+module.exports.config = {
+    name: "ping",
+    description: "Tells you the bot latency!",
+    usage: "++ping",
+    aliases: []
+};
