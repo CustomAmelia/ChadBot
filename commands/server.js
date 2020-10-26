5c6894379e538433d3509eaf843f5650582aad4d
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-26 11:31:50 +0000

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("Join our discord server here! https://discord.gg/AEGRMSS")
+    message.channel.send("Join our discord server here! https://discord.gg/AEGRMSS (THIS SECTION IS TO MAKE SURE SOMETHING WORKS)")
 }
 
 module.exports.config = {
