49f12329e3bc59d84302c498bd09d38b63735cb2
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-27 19:01:47 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,39 +2,11 @@ const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
     if (message.author.id !== '498097065264676864') return;
-    const embed = new Discord.MessageEmbed()
-        .setTitle('Evaluating...')
-    const msg = await message.channel.send(embed);
-    try {
-        const data = eval(args.join(' ').replace(/```/g, ''));
-        const embed = new Discord.MessageEmbed()
-            embed.setTitle('Output: ')
-            if (!data) {
-                embed.setDescription("No code to eval!")
-            }
-            embed.setDescription(await data)
-        await msg.edit(embed)
-        await msg.react('✅')
-        await msg.react('❌')
-        const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
-        msg.awaitReactions(filter, { max: 1 })
-            .then((collected) => {
-                collected.map((emoji) => {
-                    switch (emoji._emoji.name) {
-                        case '✅':
-                            msg.reactions.removeAll();
-                            break;
-                        case '❌':
-                            msg.delete()
-                            break;
-                    }
-                })
-            })
-    } catch (e) {
-        const embed = new Discord.MessageEmbed()
-            .setTitle('An Error has occured')
-        return await msg.edit(embed);
-    }
+
+    const { member, channel, content } = message
+
+      const result = args.join(' ')
+      channel.send(result)
 }
 module.exports.config = {
     name: "eval",
