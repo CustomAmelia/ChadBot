f9f2de3c7dfc026819d363b993427f363a7aa4dd
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-27 19:37:05 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,16 +1,32 @@
 const Discord = require('discord.js')
+const { inspect } = require('util')
+const { stripIndents } = require('common-tags')
+const { VultrexHaste } = require('vultrex.haste')
+const haste = new VultrexHaste({ url: "https://hasteb.in/" })
 
 module.exports.run = async (bot, message, args) => {
     if (message.author.id !== '498097065264676864') return;
 
-    const { member, channel, content } = message
+    if (!args[0]) return message.channel.send('Please provide javascript code to run.')
 
-      const result = eval(args.join(' ')).catch(err => {
-        return message.channel.send(`ERROR: ${err}`)
-    })
+    try {
+        const start = process.hrtime()
+        let output = eval(args.join(' '))
+        const difference = process.hrtime(start)
+        if (typeof output !== "string") output = inspect(output, { depth: 2 })
 
-      if (!result) return;
-      channel.send(result)
+        return message.channel.send(stripIndents`
+        *Executed In ${difference[0] > 0 ?`${difference[0]}s ` : ""} ${difference[1] / 1e6}ms*
+        \`\`\`js
+        ${output.length > 1950 ? await haste.post(output) : output}
+        \`\`\`
+        `)
+    } catch(err) {
+        return message.channel.send(stripIndents`
+        Error:
+        \`${err}\`
+        `)
+    }
 }
 module.exports.config = {
     name: "eval",
