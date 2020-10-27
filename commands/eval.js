e13cb642006fbfa2a2fcbae1c23eb804cecf704e
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-27 18:53:08 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,26 +1,38 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id !== "498097065264676864") return;
-      const clean = text => {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-      return text;
-}
-        try {
-
-      const code = args.slice().join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {code:"xl"});
-    } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    if (message.author.id !== '498097065264676864') return;
+    const embed = new MessageEmbed()
+        .setTitle('Evaluating...')
+    const msg = await message.channel.send(embed);
+    try {
+        const data = eval(args.join(' ').replace(/```/g, ''));
+        const embed = new MessageEmbed()
+            .setTitle('Output: ')
+            .setDescription(await data)
+        await msg.edit(embed)
+        await msg.react('✅')
+        await msg.react('❌')
+        const filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && (user.id === message.author.id);
+        msg.awaitReactions(filter, { max: 1 })
+            .then((collected) => {
+                collected.map((emoji) => {
+                    switch (emoji._emoji.name) {
+                        case '✅':
+                            msg.reactions.removeAll();
+                            break;
+                        case '❌':
+                            msg.delete()
+                            break;
+                    }
+                })
+            })
+    } catch (e) {
+        const embed = new MessageEmbed()
+            .setTitle('An Error has occured')
+        return await msg.edit(embed);
     }
 }
-
 module.exports.config = {
     name: "eval",
     description: "Evaluates code!",
