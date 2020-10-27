a2c15f766b35854bc01c38e8e41804bbb21c2d54
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-27 21:40:15 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -4,7 +4,7 @@ const { stripIndents } = require('common-tags')
 const { VultrexHaste } = require('vultrex.haste')
 const haste = new VultrexHaste({ url: "https://hasteb.in" })
 
-module.exports.run = async (bot, message, args, delay) => {
+module.exports.run = async (bot, message, args) => {
     if (message.author.id !== '498097065264676864') return;
 
     if (!args[0]) return message.channel.send('Please provide javascript code to run.')
@@ -15,8 +15,6 @@ module.exports.run = async (bot, message, args, delay) => {
         const difference = process.hrtime(start)
         if (typeof output !== "string") output = inspect(output, { depth: 2 })
 
-        await delay(10)
-        
         message.channel.send(stripIndents`
         *Executed In ${difference[0] > 0 ?`${difference[0]}s ` : ""} ${difference[1] / 1e6}ms*
         \`\`\`js
