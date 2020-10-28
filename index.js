13a43cb7e7e38262da0c6138e615497bd0da15c4
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-28 04:35:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -84,7 +84,9 @@ for (const line of eachLine) {
   if (line.includes('=')) {
     const split = line.split('=')
     const emoji = split[0].trim()
-    message.react(emoji)
+    message.react(emoji).catch(err => {
+        return;
+    })
   }
 }
 })
