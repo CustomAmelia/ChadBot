023c675959f930cda05c1a0d4931d00bf0c600a1
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-28 03:30:23 +0000

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -9,27 +9,16 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send(
           `You do not have the correct permissions to run this command, ${message.author.username}`
         );
-      const channel =
-        message.mentions.channels.first() ||
-        message.guild.channels.cache.get(args[0]);
-      if (!channel) {
-        return message.channel.send(
-          `You did not mention / give the id of your channel!`
-        );
-      }
-      let question = args.join(' ')
-        .split(`${bot.prefix}poll ${channel} `)
-        .join("");
-      if (!question)
-        return message.channel.send(`You did not specify your question!`);
-      const Embed = new Discord.MessageEmbed()
-        .setTitle(`New poll!`)
-        .setDescription(`${question}`)
-        .setFooter(`${message.author.username} created this poll.`)
-        .setColor(`RANDOM`);
-      let msg = await bot.channels.cache.get(channel.id).send(Embed);
-      await msg.react("👍");
-      await msg.react("👎");
+
+        const topic = args.join(' ')
+        const embed = new Discord.MessageEmbed()
+        .setTitle('New Poll')
+        .setColor("RANDOM")
+        .setDescription(topic)
+        .setFooter(`Started by ${message.author}`)
+        const mes = await message.channel.send(embed)
+        await mes.react("👍")
+        await mes.react("👎")
     }
     usedCommand.add(message.author.id);
     setTimeout(() => {
