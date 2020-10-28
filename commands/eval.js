f0f79d7181b56b0636e248d9b8a6a39bbc19bfe3
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-28 00:18:35 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -11,9 +11,11 @@ module.exports.run = async (bot, message, args) => {
 
     try {
         const start = process.hrtime() 
-        let output = eval(args.join(' ')).catch(err => message.channel.send(err))
+        let output = eval(args.join(' ')).catch(err => {
+            return message.channel.send(err)
+        })
         const difference = process.hrtime(start)
-        if (typeof output !== "string") output = inspect(output, { depth: 1 })
+        if (typeof output !== "string") output = inspect(output, { depth: 5 })
 
         message.channel.send(stripIndents`
         *Executed In ${difference[0] > 0 ?`${difference[0]}s ` : ""} ${difference[1] / 1e6}ms*
