8f8558572285cbdb1135b040541453d8622afc20
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-28 04:09:43 +0000

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const usedCommand = new Set()
+const prefix = require('../models/prefix');
 const userCreatedPolls = new Map();
 
 module.exports.run = async (bot, message, args, delay) => {
@@ -11,92 +12,35 @@ module.exports.run = async (bot, message, args, delay) => {
           `You do not have the correct permissions to run this command, ${message.author.username}`
         );
 
-        function getPollOptions(collector) {
-            return new Promise((resolve, reject) => {
-                collector.on('end', collected => resolve(collected.map(m => m.content.toLowerCase())));
-            });
-        }
+        const data = await prefixModel.findOne({
+            GuildID: message.guild.id
+        });
 
-        function processPollResults(voteCollector, pollOptions, userVotes, pollTally) {
-            return new Promise((resolve, reject) => {
-                voteCollector.on('collect', msg => {
-                    let option = msg.content.toLowerCase();
-                    if(!userVotes.has(msg.author.id) && pollOptions.includes(option)) {
-                        userVotes.set(msg.author.id, msg.content);
-                        let voteCount = pollTally.get(option);
-                        pollTally.set(option, ++voteCount);
-                    }
-                });
-                voteCollector.on('end', collected => {
-                    console.log("Collected " + collected.size + " votes.");
-                    resolve(collected);
-                })
-            });
-        }
-
-        if(userCreatedPolls.has(message.author.id)) {
-            message.channel.send("You already have a poll going on right now.");
-            return;
-        }
-        message.channel.send("Enter options. Max 5. Type done when finished.");
-        let filter = m => {
-            if(m.author.id === message.author.id) {
-                if(m.content.toLowerCase() === 'done') collector.stop();
-                else return true;
-            }
-            else return false;
-        }
-        let collector = message.channel.createMessageCollector(filter, { maxMatches: 5 });
-        let pollOptions = await getPollOptions(collector);
-        if(pollOptions.length < 2) {
-            message.channel.send("Not enough options, must contain 2!");
-            return;
-        }
-        let embed = new Discord.MessageEmbed();
-        embed.setTitle("Your Poll");
-        embed.setDescription(pollOptions.join("\n"));
-        let confirm = await message.channel.send(embed);
-        
-        await confirm.react('✅');
-        await confirm.react('❎');
-
-        let reactionFilter = (reaction, user) => (user.id === message.author.id) && !user.bot;
-        let reaction = (await confirm.awaitReactions(reactionFilter, { max: 1 })).first();
-        if(reaction.emoji.name === '✅') {
-            message.channel.send("Poll will begin in 1 seconds.");
-            await delay(1);
-            message.channel.send("Vote now!");
-            let userVotes = new Map();
-            let pollTally = new discord.Collection(pollOptions.map(o => [o, 0]));
-            let pollFilter = m => !m.bot;
-            let voteCollector = message.channel.createMessageCollector(pollFilter, {
-                time: 60000
-            });
-            userCreatedPolls.set(message.author.id, voteCollector);
-            await processPollResults(voteCollector, pollOptions, userVotes, pollTally);
-            let max = Math.max(...pollTally.array());
-            console.log(pollTally.entries());
-            let entries = [...pollTally.entries()];
-            let winners = [];
-            let embed = new Discord.MessageEmbed();
-            let desc = '';
-            entries.forEach(entry => entry[1] === max ? winners.push(entry[0]) : null);
-            entries.forEach(entry => desc  += entry[0] + " received " + entry[1] + " votes(s)\n");
-            embed.setDescription(desc);
-
-            if(winners.length === 1) {
-                message.channel.send(winners[0] + " is the winner!", embed);
-            }
-            else {
-                message.channel.send("We have a draw!", embed);
-            }
-        }
-        else if(reaction.emoji.name === '❎') {
-            message.channel.send("Poll cancelled.");
-        }
-        await delay(20)
-            userCreatedPolls.get(message.author.id).stop();
-             userCreatedPolls.delete(message.author.id);
+        message.mentions.channels.first() ||
+        message.guild.channels.cache.get(args[0]);
+      if (!channel) {
+        return message.channel.send(
+          `You did not mention / give the id of your channel!`
+        );
+      }
+      let question = message.content
+      if (!data) {
+        question.split(`++poll ${channel} `) 
+      }
+      else if (data) {
+        question.split(`${data.prefix}poll ${channel} `) 
+      }
+        question.join("");
+      if (!question)
+        return message.channel.send(`You did not specify your question!`);
+      const Embed = new Discord.MessageEmbed()
+        .setTitle(`New poll!`)
+        .setDescription(`${question}`)
+        .setFooter(`${message.author.username} created this poll.`)
+        .setColor(`RANDOM`);
+      let msg = await bot.channels.cache.get(channel.id).send(Embed);
+      await msg.react("👍");
+      await msg.react("👎");
     }
 }
     usedCommand.add(message.author.id);
