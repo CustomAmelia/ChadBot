e369817a3d43607726269a0ec86a89a600a20c8a
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-28 16:27:24 +0000

    Update botstats.js

--- commands/botstats.js
+++ commands/botstats.js
@@ -9,10 +9,13 @@ module.exports.run = async (bot, message, args) => {
     let hours = Math.floor(bot.uptime / 3600000) % 24;
     let minutes = Math.floor(bot.uptime / 60000) % 60;
     let seconds = Math.floor(bot.uptime / 1000) % 60;
+    const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+    arr.reverse();
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
         const embed = new Discord.MessageEmbed()
         .setTitle('Bot Stats')
         .setColor("RANDOM")
-        .addField('Memory Usage', process.memoryUsage().heapUsed / 1024 / 1024, true)
+        .addField('Memory Usage', `${Math.round(used * 100) / 100}MB`, true)
         .addField('Uptime', `${days}d ${hours}h ${minutes}m ${seconds}s`, true)
         .addField('Guilds', bot.guilds.cache.size, true)
         .addField('Developer', "<@498097065264676864>", true)
