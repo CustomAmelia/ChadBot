202782ea736dffad1649b1bbb2e11a379fbd6702
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-30 16:14:07 +0000

    Create commits.js

---
+++ commands/commits.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const {
+    stripIndents
+} = require('common-tags')
+
+module.exports.run = async (bot, message, args) => {
+    let server = bot.guilds.cache.get('771048912881844244')
+    let channel = server.channels.cache.get('771048912881844244').latest
+
+    channel
+    .fetchMessages({ limit: 1 })
+    .then(f =>
+      message.channel.send(
+        f.first().content
+      )
+    );
+}
+module.exports.config = {
+    name: "commits",
+    description: "Shows the latest commits!",
+    usage: "++commits",
+    aliases: []
+};
