a574fc998ddb88a9a0a0039cfbc9e4b9673f0c04
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-30 20:10:42 +0000

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -7,37 +7,54 @@ module.exports.run = async (bot, message, args) => {
     if (usedCommand.has(message.author.id)) {
         message.reply('Slow down! You have to wait 2 seconds to use this command again.')
     } else {
-        let userArray = message.content.split(" ");
-        let userArgs = userArray.slice(1);
-        let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
-    
-        if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
-        if (member.presence.status === 'online') member.presence.status = 'Online';
-        if (member.presence.status === 'idle') member.presence.status = 'Idle';
-        if (member.presence.status === 'offline') member.presence.status = 'offline';
-    
-        let x = Date.now() - member.createdAt;
-        let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
-        const joined = Math.floor(y / 86400000);
-    
-        const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss");
-        let status = member.presence.status;
-    
-        const userEmbed = new Discord.MessageEmbed()
-        .setAuthor(member.user.tag, member.user.displayAvatarURL())
-        .setTimestamp()
-        .setColor('RANDOM')
-        .setImage(member.user.displayAvatarURL())
-        .addField("Member ID", member.id)
-        .addField('Roles', `<@&${member._roles.join('> <@&')}>`)
-        .addField("Account Created On:", ` ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-        .addField('Joined the server At', `${joineddate} \n> ${joined} day(S) Ago`)
-        .addField("Status", status)
-        if(settings.staff.includes(member.id)) {
-            userEmbed.setFooter('This user is apart of the ChadBot staff team!')
-            message.channel.send(userEmbed);
-        }
-        else message.channel.send(userEmbed);
+        const flags = {
+            DISCORD_EMPLOYEE: 'Discord Employee',
+            DISCORD_PARTNER: 'Discord Partner',
+            BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
+            BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
+            HYPESQUAD_EVENTS: 'HypeSquad Events',
+            HOUSE_BRAVERY: 'House of Bravery',
+            HOUSE_BRILLIANCE: 'House of Brilliance',
+            HOUSE_BALANCE: 'House of Balance',
+            EARLY_SUPPORTER: 'Early Supporter',
+            TEAM_USER: 'Team User',
+            SYSTEM: 'System',
+            VERIFIED_BOT: 'Verified Bot',
+            VERIFIED_DEVELOPER: 'Verified Bot Developer'
+        };
+
+        const member = message.mentions.members.last() || message.guild.members.cache.get(target) || message.member;
+		const roles = member.roles.cache
+			.sort((a, b) => b.position - a.position)
+			.map(role => role.toString())
+			.slice(0, -1);
+		const userFlags = member.user.flags.toArray();
+		const embed = new MessageEmbed()
+			.setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+			.setColor(member.displayHexColor || 'BLUE')
+			.addField('User', [
+				`**❯ Username:** ${member.user.username}`,
+				`**❯ Discriminator:** ${member.user.discriminator}`,
+				`**❯ ID:** ${member.id}`,
+				`**❯ Flags:** ${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`,
+				`**❯ Avatar:** [Link to avatar](${member.user.displayAvatarURL({ dynamic: true })})`,
+				`**❯ Time Created:** ${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`,
+				`**❯ Status:** ${member.user.presence.status}`,
+				`**❯ Game:** ${member.user.presence.game || 'Not playing a game.'}`,
+				`\u200b`
+			])
+			.addField('Member', [
+				`**❯ Highest Role:** ${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest.name}`,
+				`**❯ Server Join Date:** ${moment(member.joinedAt).format('LL LTS')}`,
+				`**❯ Hoist Role:** ${member.roles.hoist ? member.roles.hoist.name : 'None'}`,
+				`**❯ Roles [${roles.length}]:** ${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : 'None'}`,
+				`\u200b`
+            ]);
+            if(settings.staff.includes(member.id)) {
+                embed.setFooter('This user is apart of the ChadBot Staff Team!')
+                message.channel.send(embed);
+            }
+            else return message.channel.send(embed);
     }
     usedCommand.add(message.author.id)
     setTimeout(() => {
