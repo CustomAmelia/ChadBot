a1fb3afa23e6fac1c2117df0cab2d51129e29fb2
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-31 21:37:23 +0000

    Update djsdocs.js

--- commands/djsdocs.js
+++ commands/djsdocs.js
@@ -3,17 +3,30 @@ const settings = require('../botsettings.json');
 const fetch = require('node-fetch')
 const usedCommand = new Set()
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args, delay) => {
     if (usedCommand.has(message.author.id)) {
         message.reply('Slow down! You have to wait 2 seconds to use this command again.')
     } else {
         if (!args[0]) return message.channel.send('Please specify a query.')
-        const queryParams = new URLSearchParams({ src: 'stable', q: args[0] })
+        if (!args[1]) {
+            let src = await message.channel.send("No src provided! Will search using the default 'stable' one. This message will be deleted in 4 seconds.")
+            await delay(4)
+            src.delete()
+            const queryParams = new URLSearchParams({ src: 'stable', q: args[0] })
 
-        const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryParams.toString()}`)
-        .then(res => res.json())
-  
-      message.channel.send({ embed })
+            const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryParams.toString()}`)
+            .then(res => res.json())
+      
+          message.channel.send({ embed })
+        }
+        else if (args[1]) {
+            const queryParams = new URLSearchParams({ src: args[1], q: args[0] })
+
+            const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryParams.toString()}`)
+            .then(res => res.json())
+      
+          message.channel.send({ embed })
+        }
     }
     usedCommand.add(message.author.id)
     setTimeout(() => {
