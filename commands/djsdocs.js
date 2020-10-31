a9b910e729f6c17a619de1e999a01a55035c3557
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-10-31 20:55:01 +0000

    djsdocs work

---
+++ commands/djsdocs.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js')
+const settings = require('../botsettings.json');
+const usedCommand = new Set()
+
+module.exports.run = async (bot, message, args, { channel }, query) => {
+    if (usedCommand.has(message.author.id)) {
+        message.reply('Slow down! You have to wait 2 seconds to use this command again.')
+    } else {
+        const queryParams = new URLSearchParams({ src: 'stable', q: query })
+
+        const embed = await fetch(`https://djsdocs.sorta.moe/v2/embed?${queryParams.toString()}`)
+        .then(res => res.json())
+  
+      channel.send({ embed })
+    }
+    usedCommand.add(message.author.id)
+    setTimeout(() => {
+        usedCommand.delete(message.author.id);
+    }, 2000);
+}
+
+module.exports.config = {
+    name: "djsdocs",
+    description: "Retrivies discord.js docs.",
+    usage: "++djsdocs <info>",
+    aliases: []
+};
