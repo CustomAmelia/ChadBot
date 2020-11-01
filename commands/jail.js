2d4921be233cce7e77550adc42dab8cc218fd40a
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-01 20:46:14 +0000

    Update jail.js

--- commands/jail.js
+++ commands/jail.js
@@ -7,11 +7,11 @@ module.exports.run = async (bot, message, args) => {
         message.reply('Slow down! You have to wait 2 seconds to use this command again.')
     }
     else {
-        const user = await bot.resolveUser(args[0]) || message.author;
+        const user = await this.client.resolveUser(args[0]) || message.author;
         const m = await message.sendT("misc:PLEASE_WAIT", null, {
             prefixEmoji: "loading"
         });
-        const buffer = await bot.AmeAPI.generate("jail", { url: user.displayAvatarURL({ format: "png", size: 1024 }) });
+        const buffer = await this.client.AmeAPI.generate("jail", { url: user.displayAvatarURL({ format: "png", size: 1024 }) });
         const attachment = new Discord.MessageAttachment(buffer, "jail.png");
         m.delete();
         message.channel.send(attachment);
