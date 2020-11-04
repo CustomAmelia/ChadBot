cac7f201cbd3a00a36a1ab496b90d92fd5ac5661
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-04 19:52:25 +0000

    CanvaCord + Rank Cards

--- commands/rank.js
+++ commands/rank.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js')
 const Levels = require('discord-xp')
+const canvacord = require('canvacord')
+const data = require('canvacord/src/Plugins')
 const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
@@ -9,16 +11,52 @@ module.exports.run = async (bot, message, args) => {
     } else {
         const person = message.mentions.users.first()
         if (!person) {
-            const embed = new Discord.MessageEmbed()
             const user = await Levels.fetch(message.author.id, message.guild.id);
-            embed.setTitle(`${message.author.username}'s Rank!`)
-            embed.setColor('RANDOM')
-            embed.setDescription(`• **Level:** ${user.level} \n • **XP:** ${user.xp}`)
-            message.channel.send(embed)
+            const neededXp = Levels.xpFor(parseInt(user.level) + 1)
+            if (message.author.user.presence.status === 'dnd') message.author.user.presence.status = 'Do Not Disturb';
+            if (message.author.user.presence.status === 'online') message.author.user.presence.status = 'Online';
+            if (message.author.user.presence.status === 'idle') message.author.user.presence.status = 'Idle';
+            if (message.author.user.presence.status === 'offline') message.author.user.presence.status = 'Offline';
+            const rank = new canvacord.Rank()
+                .setAvatar(message.author.displayAvatarURL({
+                    dynamic: false,
+                    format: 'png'
+                }))
+                .setCurrentXP(user.xp)
+                .setRequiredXP(neededXp)
+                .setStatus(message.author.user.presence.status)
+                .setProgressBar('#FFA500', "COLOR")
+                .setUsername(message.author.username)
+                .setDiscriminator(message.author.user.discriminator)
+            rank.build()
+                .then(data => {
+                    const attachment = new Discord.MessageAttachment(data, 'rank.png')
+                    message.channel.send(attachment)
+                })
         } else if (person) {
             if (person.bot) return;
-            const embed = new Discord.MessageEmbed()
             const user = await Levels.fetch(person.id, message.guild.id);
+            const neededXp = Levels.xpFor(parseInt(user.level) + 1)
+            if (person.user.presence.status === 'dnd') person.user.presence.status = 'Do Not Disturb';
+            if (person.user.presence.status === 'online') person.user.presence.status = 'Online';
+            if (person.user.presence.status === 'idle') person.user.presence.status = 'Idle';
+            if (person.user.presence.status === 'offline') person.user.presence.status = 'Offline';
+            const rank = new canvacord.Rank()
+                .setAvatar(person.user.displayAvatarURL({
+                    dynamic: false,
+                    format: 'png'
+                }))
+                .setCurrentXP(user.xp)
+                .setRequiredXP(neededXp)
+                .setStatus(person.user.presence.status)
+                .setProgressBar('#FFA500', "COLOR")
+                .setUsername(person.user.username)
+                .setDiscriminator(person.user.discriminator)
+            rank.build()
+                .then(data => {
+                    const attachment = new Discord.MessageAttachment(data, 'rank.png')
+                    message.channel.send(attachment)
+                })
             embed.setTitle(`${person.username}'s Rank!`)
             if (!user.xp) {
                 embed.setDescription('• **Level:** 0 \n • **XP:** 0')
