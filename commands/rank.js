f4e19c3f8d20492f2444b21323c546e13ce171d4
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-04 20:30:25 +0000

    Update rank.js

--- commands/rank.js
+++ commands/rank.js
@@ -25,7 +25,6 @@ module.exports.run = async (bot, message, args) => {
                 .setUsername(message.author.username)
                 .setDiscriminator(message.author.discriminator)
                 .setLevel(user.level)
-                .setRank(user.rank)
             rank.build()
                 .then(data => {
                     const attachment = new Discord.MessageAttachment(data, 'rank.png')
