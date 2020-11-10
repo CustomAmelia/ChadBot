41cd3bb62f0e0e8ef136c0783ce7700b15bf3c05
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-10 20:57:16 +0000

    Update cmdinfo.js

--- commands/cmdinfo.js
+++ commands/cmdinfo.js
@@ -12,9 +12,9 @@ module.exports.run = async (bot, message, args) => {
     if(helpArgs[0]) {
       let command = helpArgs[0];
 
-      if(bot.commands(command).toLowerCase()) {
+      if(bot.commands.toLowerCase()(command)) {
           
-          command = bot.commands.get(command).toLowerCase()
+          command = bot.commands.toLowerCase().get(command)
           var embed = new Discord.MessageEmbed()
           .setAuthor(`${command.config.name} Command`)
           .addField('Description:', `${command.config.description || "There is no description for this command."} `)
