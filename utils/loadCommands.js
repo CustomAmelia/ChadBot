9cfdc54f70734d35e0dfe76e5e520c232fc6a005
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-13 19:32:50 +0000

    Update loadCommands.js

--- utils/loadCommands.js
+++ utils/loadCommands.js
@@ -4,11 +4,11 @@ function loadCommands(bot) {
     fs.readdir("./commands/", (error, Category) => {
         if (error) throw error;
         Category.forEach((category) => {
-          fs.readdir(`./commands/${category}`, (err, commands) => {
+          fs.readdir(`../commands/${category}`, (err, commands) => {
             if (err) throw err;
       
             commands.forEach((command) => {
-              const cmd = require(`./commands/${category}/${command}`);
+              const cmd = require(`../commands/${category}/${command}`);
       
               client.commands.set(cmd.config.name, cmd);
               cmd.config.aliases.forEach((alias) => {
