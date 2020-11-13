cf627d7cae8979d43f3e1fd0c1aa10530c50c9ab
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-13 20:02:29 +0000

    Update djsdocs.js

--- src/commands/dev/djsdocs.js
+++ src/commands/dev/djsdocs.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js')
-const settings = require('../../botsettings.json');
+const settings = require('../../../botsettings.json');
 const fetch = require('node-fetch')
 const usedCommand = new Set()
 
