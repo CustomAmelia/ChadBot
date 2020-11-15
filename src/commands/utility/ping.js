aadb9988cdaed38d65ed5448223be85ab71352d9
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-15 12:44:16 +0000

    Update ping.js

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -2,12 +2,17 @@ const Discord = require('discord.js')
 const usedCommand = new Set()
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("Pinging...").then(m => {
-        var ping = m.createdTimestamp - message.createdTimestamp;
-        var botPing = Math.round(bot.pi);
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Pinging...")
 
-        m.edit(`*Pong!* Bot Ping Is: ${ping}ms`);
-    });
+    const msg = await message.channel.send({ embed: embed });
+    
+    const embed2 = new Discord.MessageEmbed()
+    .setTitle("Pong!")
+    .setDescription(`API Latency: ${msg.createdTimestamp - message.createdTimestamp} ms\nBot Latency: ${bot.ws.ping} ms`)
+    .setColor("RANDOM")
+
+    msg.edit({ embed: embed2 });
 }
 
 module.exports.config = {
