8c123057a04b484f6437932e1f14eb05c28372fa
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-15 15:40:38 +0000

    Update eval.js

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -27,16 +27,7 @@ module.exports.run = async (bot, message, args) => {
         
         const embed = new Discord.MessageEmbed()
         .setTitle(`Executed In ${difference[0] > 0 ?`${difference[0]}s ` : ""} ${difference[1] / 1e6}ms`)
-        .addField(stripIndents `
-        Input:
-        \`\`\`js
-        ${args.join(' ')}\`\`\`
-        `)
-        .addField(stripIndents `
-        Output:
-        \`\`\`js
-        ${output.length > 1950 ? await haste.post(output) : output}\`\`\`
-        `)
+        .setDescription(`Input: \n \`\`\`js\n${args.join(' ')}\`\`\` \n Output: \n \`\`\`js\n${output.length > 1950 ? await haste.post(output) : output}\`\`\``)
         .setColor("RANDOM")
 
         message.channel.send(embed)
