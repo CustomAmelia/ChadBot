3b3c29d383301024719c93d1334641c836bf2148
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-15 15:27:34 +0000

    Update eval.js

--- src/commands/owner/eval.js
+++ src/commands/owner/eval.js
@@ -29,11 +29,13 @@ module.exports.run = async (bot, message, args) => {
         .setTitle(`Executed In ${difference[0] > 0 ?`${difference[0]}s ` : ""} ${difference[1] / 1e6}ms`)
         .addField('Input', args.join(' '))
         .addField('Output', `\`\`\`js \n ${output.length > 1950 ? await haste.post(output) : output}\`\`\``)
+        .setColor("RANDOM")
 
         message.channel.send(embed)
     } catch (err) {
         const embed = new Discord.MessageEmbed()
-        .setTitle('ERROR')
+        .setTitle('Oops! Something went wrong.')
+        .setColor("RANDOM")
         .setDescription(stripIndents `
         Error:
         \`\`\`js
