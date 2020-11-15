7b7e81d4ac8816e2ef34c2358e88302762ece13f
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-15 12:45:38 +0000

    Update ping.js

--- src/commands/utility/ping.js
+++ src/commands/utility/ping.js
@@ -4,13 +4,14 @@ const usedCommand = new Set()
 module.exports.run = async (bot, message, args) => {
     const embed = new Discord.MessageEmbed()
     .setTitle("Pinging...")
+    .setColor("#8b73f4")
 
     const msg = await message.channel.send({ embed: embed });
     
     const embed2 = new Discord.MessageEmbed()
     .setTitle("Pong!")
     .setDescription(`API Latency: ${msg.createdTimestamp - message.createdTimestamp} ms\nBot Latency: ${bot.ws.ping} ms`)
-    .setColor("RANDOM")
+    .setColor("#8b73f4")
 
     msg.edit({ embed: embed2 });
 }
