dc530058efa08e422028faa9029415cd145b2bed
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-15 14:57:46 +0000

    Create say.js

---
+++ src/commands/owner/say.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const usedCommand = new Set()
+
+module.exports.run = async (bot, message, args) => {
+    const msg = args.join(' ')
+
+    if (!msg) return;
+    if (!message.author.id === '498097065264676864') return;
+
+    message.channel.send(msg)
+}
+
+module.exports.config = {
+    name: "say",
+    description: "Sends the message you tell it to!",
+    usage: "++say <msg>",
+    aliases: []
+};
