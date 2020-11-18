5cc8f7ddf36af7f64c455b2f3fee647226aa3fd2
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-11-18 23:12:45 +0000

    Update rank.js

--- src/commands/xp/rank.js
+++ src/commands/xp/rank.js
@@ -13,17 +13,12 @@ module.exports.run = async (bot, message, args) => {
         if (!person) {
             const user = await Levels.fetch(message.author.id, message.guild.id);
             const neededXp = Levels.xpFor(parseInt(user.level) + 1)
-            let image = ""
-            if (message.guild.id === '567809387226464256') {
-                image = "https://media.discordapp.net/attachments/573978262754164761/752946028180799568/IMG_20200908_123833452.jpg?width=425&height=567"
-            }
-            else image = "https://i.imgur.com/5QHCaOQ.png"
             const rank = new canvacord.Rank()
                 .setAvatar(message.author.displayAvatarURL({
                     dynamic: false,
                     format: 'png'
                 }))
-                .setBackground("IMAGE", image)
+                .setBackground("IMAGE", 'https://i.imgur.com/5QHCaOQ.png')
                 .setCurrentXP(user.xp)
                 .setRequiredXP(neededXp)
                 .setStatus(message.member.user.presence.status)
@@ -41,18 +36,13 @@ module.exports.run = async (bot, message, args) => {
             if (person.bot) return;
             const user = await Levels.fetch(person.id, message.guild.id);
             const neededXp = Levels.xpFor(parseInt(user.level) + 1)
-            let image = ""
-            if (message.guild.id === '567809387226464256') {
-                image = "https://media.discordapp.net/attachments/573978262754164761/752946028180799568/IMG_20200908_123833452.jpg?width=425&height=567"
-            }
-            else image = "https://i.imgur.com/5QHCaOQ.png"
             const rank = new canvacord.Rank()
                 .setAvatar(person.displayAvatarURL({
                     dynamic: false,
                     format: 'png'
                 }))
                 .setCurrentXP(user.xp)
-                .setBackground("IMAGE", image)
+                .setBackground("IMAGE", 'https://i.imgur.com/5QHCaOQ.png')
                 .setRequiredXP(neededXp)
                 .setStatus(person.presence.status)
                 .setProgressBar('#8b73f4', "COLOR")
